4d9376fe540273eed819d0d49932293052c8b68d
ffremont <ff.fremont.florent@gmail.com>
2014-07-23 18:11:45 +0200

    amélioration perfInterceptor bean

--- src/main/php/Huge/Rest/Interceptors/PerfInterceptor.php
+++ src/main/php/Huge/Rest/Interceptors/PerfInterceptor.php
@@ -3,7 +3,10 @@
 namespace Huge\Rest\Interceptors;
 
 use Huge\IoC\Annotations\Component;
+use Huge\IoC\Annotations\Autowired;
 use Huge\Rest\Process\IInterceptor;
+
+use Huge\Rest\Http\HttpRequest;
 
 /**
  * @Component
@@ -25,6 +28,12 @@ class PerfInterceptor implements IInterceptor{
     private $memoryStart;
     
     /**
+     * @Autowired("Huge\Rest\Http\HttpRequest")
+     * @var \Huge\Rest\Http\HttpRequest 
+     */
+    private $request;
+    
+    /**
      *
      * @var \Logger
      */
@@ -39,6 +48,10 @@ class PerfInterceptor implements IInterceptor{
         $memoryPeak= memory_get_peak_usage() / 1048576;
         $memoryPeak= memory_get_peak_usage() / 1048576;
         
+        if($this->request !== null){
+            $this->logger->info('Performance de la ressource : '.$this->request->getUri());
+        }
+        
         $this->logger->info('Temps d\'exécution de la requête pendant '.round($time, 2).' ms');
         $this->logger->info('Consommation de '.$this->memoryStart.' mo, avec un pic à '.$memoryPeak.' mo');
     }
@@ -47,6 +60,15 @@ class PerfInterceptor implements IInterceptor{
         $this->startTime = microtime(true);
         $this->memoryStart = memory_get_usage() / 1048576;
     }
+
+    public function getRequest() {
+        return $this->request;
+    }
+
+    public function setRequest(\Huge\Rest\Http\HttpRequest $request) {
+        $this->request = $request;
+    }
+
 
 }
 
